2d367da78094dd6614be39906287903150b2de5d
chumpon-asaneerat <chumponsenate@gmail.com>
2019-07-08 13:59:23 +0700

    Add required packages (for websql) and create db

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-adapter-node-websql'));
+
 console.log('pouchdb sample start.');
+
+var db = new PouchDB('databases/sampledb', {adapter: 'websql'});
+db.info().then((info) => {
+    console.log(info);
+})
 
 console.log('pouchdb sample end.');
